70bd8d2459c6739c1e505420c3e04a9d78b2973b
noodles-dev <simi.simecek@seznam.cz>
2023-04-17 19:18:55 +0200

    Add promer github links

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # A news app written in Next.js
 This is little news app hosted on Vercel
-[`Website`](https://vercel.app)
+[`Website`](https://simon-news.vercel.app)
--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ return (
         <div className={styles.main}>
             <div onClick={() => router.push('/')}>Home</div>
             <div onClick={() => router.push('/feed/1')}>Feed</div>
-            <div onClick={() => (window.location.href = 'https://github.com/ssimondev')}>Github</div>
+            <div onClick={() => (window.location.href = 'https://github.com/ssimondev/next-news')}>Github</div>
         </div>
     )
 }
