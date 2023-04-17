36755a1d05691739b5eaf43e1ed592b23cddaf15
noodles-dev <simi.simecek@seznam.cz>
2023-04-17 19:03:40 +0200

    Edit the title screen

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,12 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
       <div className={styles.main}>
-        <h1 className={styles.title}><span>News</span> App</h1>
-        <p className={styles.description}>Your one stop shop for the latest news articles</p>
+        <h1 className={styles.title}>Newspaper</h1>
+        <p className={styles.pronouciation}>noo͞z′pā″pər, nyoo͞z′-</p>
+        <p className={styles.description}>Containing intelligence or reports of passing events, issued at short but regular intervals, and either sold or distributed gratis; a public print, or daily, weekly, or semi-weekly periodical, that presents the news of the day, such as the doings of political, legislative, or other public bodies, local, provincial, or national current events, items of public interest on science, religion, commerce, as well as trade, market, and money reports, advertisements and announcements, etc.</p>
       </div>
+    </>
   )
 }
