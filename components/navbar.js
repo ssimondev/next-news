1676678371ad6019b8e6580b998903ab2029035b
noodles-dev <simi.simecek@seznam.cz>
2023-04-17 17:17:28 +0200

    Version 1.0

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+import styles from '../styles/Navbar.module.css';
+const Navbar = () => {
+    const router = useRouter();
+return (
+        <div className={styles.main}>
+            <div onClick={() => router.push('/')}>Home</div>
+            <div onClick={() => router.push('/feed/1')}>Feed</div>
+            <div onClick={() => (window.location.href = 'https://github.com/ssimondev')}>Github</div>
+        </div>
+    )
+}
+export default Navbar
