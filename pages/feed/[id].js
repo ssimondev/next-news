d2cd238b058928571c39c3ba3c930e8fcc7607e9
noodles-dev <simi.simecek@seznam.cz>
2023-04-17 17:38:46 +0200

    Rounded corners

--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import styles from '../../styles/Feed.module.css';
 import { useRouter } from 'next/router';
 
